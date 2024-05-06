553bc5f2897a9e1c2225d9783e399e912d8d52b1
pass-me-dachips <da40au@gmail.com>
2024-05-06 19:50:45 +0100

    added spkg.quick helpers

---
+++ src/spkg/helpers/placehld.h.js
@@ -0,0 +1,7 @@
+
+// This file contains placeholders for the quick spkg.
+
+export const short = "The quick brown fox jumps over the lazy dog.";
+export const fill = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam";
+export const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+export const bacon = "Bacon ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
