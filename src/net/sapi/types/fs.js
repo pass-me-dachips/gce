c840a0b0cf060362aeae99b9994b7c1ae6b7a6d6
pass-me-dachips <da40au@gmail.com>
2024-05-07 12:59:56 +0100

    completed sapi FS.statics

--- src/net/sapi/types/fs.js
+++ src/net/sapi/types/fs.js
@@ -1,7 +1,7 @@
 import { join } from "node:path";
 import { code_0 } from "../codes.js";
 import { defopera } from "./default.js";
-import { dirRead } from "../opera/read.js";
+import { dirRead, getStatistics } from "../opera/read.js";
 import { ERRORCODES } from "../../../var/system.js";
 
 async function READDIR(relativePath, oid, sdu) {
@@ -15,17 +15,34 @@ async function READDIR(relativePath, oid, sdu) {
   } catch(error) {
     if (error.code  === ERRORCODES.notFound) {
       return JSON.stringify(code_0(false, "ENOENTRY", oid, null)); 
-    } else if (error.code  === ERRORCODES.notdirectory ) {
+    } else if (error.code === ERRORCODES.notdirectory ) {
       return JSON.stringify(code_0(false, "OSFORBIDEN", oid, null)); 
     } else return JSON.stringify(code_0(false, "STDERR", oid, error.message)); 
   }
 }
 
+export async function STATICS(relativePath, oid, sdu) {
+  try {
+    // the readdir expects the payload to be the relative path to the file in 
+    // which the user wants to read.
+    const { servicePath } = sdu;
+    const path = join(servicePath ,relativePath);
+    const statistics = await getStatistics(path);
+    return JSON.stringify(code_0(true, "ACK/HASPAYLOAD", oid, statistics)); 
+  } catch (error) {
+    if (error.code === ERRORCODES.notFound) {
+      return JSON.stringify(code_0(false, "ENOENTRY", oid, null));  
+    } else return JSON.stringify(code_0(false, "STDERR", oid, error.message)); 
+  }
+}
+
+// ++++++++ the main fs api ++++++++++++++++++
 export default async function fs(request,sdu) {
   let response;
   const { OPERA, PAYLOAD, OID } = request;
   switch (OPERA) {
      case "READDIR" : response = await READDIR(PAYLOAD, OID, sdu); break
+     case "STATICS" : response = await STATICS(PAYLOAD, OID, sdu); break
      default: response = defopera(OPERA);
   };
   return response;
