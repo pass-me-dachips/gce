54b09759690ed38aa4dae2fff2cbde59ef1ece96
pass-me-dachips <da40au@gmail.com>
2024-05-10 16:58:19 +0100

    updated report

--- src/etc/report.js
+++ src/etc/report.js
@@ -3,10 +3,10 @@ export function report(message, oid, type) {
   oid = `OID ${oid}`;
   const date = new Date().toUTCString();
   const fs  = `\x1b[92m${message}\x1b[0m\t\x1b[94m[${oid}]\x1b[0m\t[${date}]\n`;
-  const spkg = `\x1b[96m${message}\x1b[0m\t\x1b[94m[${oid}]\x1b[0m\t[${date}]\n`;
+  const others = `\x1b[96m${message}\x1b[0m\t\x1b[94m[${oid}]\x1b[0m\t[${date}]\n`;
   const dang = `\x1b[94m${message}\x1b[0m\t\x1b[94m[${oid}]\x1b[0m\t[${date}]\n`;
-  if (type === "spkg") process.stdout.write(spkg);
-  if (type === "danger") process.stdout.write(dang);
+  if (type === "others") process.stdout.write(others);
+  else if (type === "danger") process.stdout.write(dang);
   else process.stdout.write(fs);
   return void 0;
 }
