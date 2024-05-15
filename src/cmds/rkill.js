ada46581a8f1dbdeb83895c001c0f91f1b5f8b09
pass-me-dachips <da40au@gmail.com>
2024-05-15 13:28:59 +0100

    migrated from readline/promises(experinmental module) to readline(stable) in cmds.rkill

--- src/cmds/rkill.js
+++ src/cmds/rkill.js
@@ -1,16 +1,19 @@
-import * as readLine from "node:readline/promises";
+import * as readLine from "node:readline";
 import { end } from "../spkg/v1.0.0.js";
+import { existsAsync } from "../etc/existsAsync.js";
 import { GPATHS, GOUTFORMAT } from "../var/system.js";
 import { join } from "node:path";
-import { existsAsync } from "../etc/existsAsync.js";
 import { readFile, readdir } from "node:fs/promises";
+import { promisify } from "node:util";
+
 
 export default async function Rkill() {
   const rl = readLine.createInterface({
      input: process.stdin,
      output: process.stdout
   });
-  await rl.question("Hit enter to acknowledge this request ");
+  let rlAsync = promisify(rl.question).bind(rl);
+  await rlAsync("Hit enter to acknowledge this request ");
   rl.close();
 
   const services = await existsAsync(GPATHS.serviceLog) ?
