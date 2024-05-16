ae82a6181cf396799dc8be6fc9b7c10a2f804a6a
pass-me-dachips <da40au@gmail.com>
2024-05-16 11:04:05 +0100

    added existsAsync from swap

---
+++ src/etc/existsAsync.js
@@ -0,0 +1,19 @@
+
+"use strict";
+
+import { access, constants } from "fs/promises";
+
+/**
+ * Asynchronoulsy checks if the file exists. returns true if the path exists, 
+ * false otherwise.
+ * @param {string} path 
+ * @returns {bool}
+ */
+export async function existsAsync(path) {
+  try {
+    await access(path, constants.F_OK);
+    return true;
+  } catch(error) {
+    return false;
+  }
+}
