c4a21519ae8b91ac9a338bffef82e1820d8ba19c
pass-me-dachips <da40au@gmail.com>
2024-05-26 06:20:01 +0100

    moved handlerequest to cbs/webserver

--- src/net/www/execGcce.js
+++ src/net/www/execGcce.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 import * as readLine from "node:readline";
@@ -7,7 +6,7 @@ import { platform } from "node:os";
 import { promisify } from "node:util";
 import { supportedPlatforms } from "../../var/osPaths.js";
 
-/**
+/** a function that handles the gcce start script and execution
  * @author david, pass-me-dachips
  * @param {object} sdu the service data unit
  * @param {string} port the service port
@@ -22,30 +21,31 @@ export default async function execBrowser(sdu, port) {
     console.log(`start script > \x1b[92m${start}\x1b[0m`);
     const rl = readLine.createInterface({
       input: process.stdin,
-      output: process.stdout
+      output: process.stdout,
     });
     const message = "reply (y) to proceed or (x) to abort: ";
     let rlAsync = promisify(rl.question).bind(rl);
     let answer = await rlAsync(message);
     while (answer !== "y" && answer !== "x") {
       answer = await rlAsync(message);
-    };
+    }
     rl.close();
-    if (answer === "y") exec(start, (error, string)=> {
-      if (error) throw error;
-      console.log(string);
-    });
+    if (answer === "y")
+      exec(start, (error, string) => {
+        if (error) throw error;
+        console.log(string);
+      });
     else process.exit(0);
   } else {
     if (supportedPlatforms.includes(platform())) {
-       const proc = {
-         android: "",
-         linux: "xdg-open",
-         darwin: "open",
-         win32: "start"
-       };
-       exec(`${proc[platform()]} http://localhost:${port}`);
-     } 
+      const proc = {
+        android: "",
+        linux: "xdg-open",
+        darwin: "open",
+        win32: "start",
+      };
+      exec(`${proc[platform()]} http://localhost:${port}`);
+    }
   }
   return true;
 }
