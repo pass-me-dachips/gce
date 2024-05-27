851f837313054aab40d5076bcf5b2f4f4cd9f27f
pass-me-dachips <da40au@gmail.com>
2024-05-27 14:21:27 +0100

    updated handler for spkg requests: switched from ws to standard http

--- src/net/sapi/opera/spkg.js
+++ src/net/sapi/opera/spkg.js
@@ -1,55 +1,86 @@
 "use strict";
-//
-// import * as spkgUtils from "../../../spkg/v1.0.0.js";
-// import { code_0 } from "../codes.js";
-// import { defopera } from "./default.js";
-// import { errorResponseHelper } from "./fs.js";
-// import { join } from "node:path";
-// import { report } from "../../../etc/report.js";
 
-// const stdsignal = "ACK/HASPAYLOAD";
+import * as spkgUtils from "../../../spkg/v1.0.0.js";
+import { code_0, code_1 } from "../codes.js";
+import { errorResponseHelper } from "./fs.js";
+import finish from "../finish.js";
+import { join } from "node:path";
+import { report } from "../../../etc/report.js";
 
-// function ADDSUBDIVMULTIPLYMODPOW(type, payload, oid) {
-//   if (payload && typeof payload === "object" && payload.length !== undefined) {
-//      const value = spkgUtils[type](...payload);
-//      return JSON.stringify(code_0(true, stdsignal,oid, value));
-//   } else {
-//      const response =
-//       code_0(true, stdsignal, oid, "expected payload of type array");
-//      return JSON.stringify(response);
-//   }
-// }
+const stdsignal = "ACK/HASPAYLOAD";
 
-// function INTEGERSANDBASICBYTES(type, payload, oid) {
-//   let response = spkgUtils[type](payload);
-//   return JSON.stringify(code_0(true, stdsignal, oid, response));
-// }
+const methodpost = "expected payload of type array and method post";
+function ADDSUBDIVMULTIPLYMODPOW(type, sdu, oid, res) {
+  if (
+    sdu.method === "POST" &&
+    sdu.payload !== null &&
+    Array.isArray(sdu.payload)
+  ) {
+    const value = spkgUtils[type](...sdu.payload);
+    finish(200, code_0(true, stdsignal, oid, value), res);
+  } else finish(200, code_0(true, stdsignal, oid, methodpost), res);
+}
 
-// function HEXDECIBINRGB(type, payload, oid) {
-//   let response = spkgUtils[type](payload);
-//   return JSON.stringify(code_0(true, stdsignal, oid, response));
-// }
+function INTEGERSANDBASICBYTES(type, sdu, oid, res) {
+  if (
+    sdu.method === "POST" &&
+    sdu.payload !== null &&
+    Array.isArray(sdu.payload)
+  ) {
+    const response = spkgUtils[type](sdu.payload[0]);
+    finish(200, code_0(true, stdsignal, oid, response), res);
+  } else finish(200, code_0(true, stdsignal, oid, methodpost), res);
+}
 
-// async function QUICK(payload, oid, sdu) {
-//   try {
-//     let { path, template } = payload;
-//     if (path && template) {
-//       path = join(sdu.servicePath, path);
-//       await spkgUtils.quick(path, template);
-//       return JSON.stringify(code_0(true, "ACK", oid, null));
-//     } else throw {
-//       message : "QUICK requires the path and template fields."
-//     };
-//   } catch(error) { return errorResponseHelper(error, oid); }
-// }
+async function MARSHAL(sdu, oid, res) {
+  if (
+    sdu.method === "POST" &&
+    sdu.payload !== null &&
+    Array.isArray(sdu.payload)
+  ) {
+    const response = await spkgUtils.marshal(sdu.payload);
+    finish(200, code_0(true, stdsignal, oid, response), res);
+  } else finish(200, code_0(true, stdsignal, oid, methodpost), res);
+}
 
-// async function END(ws, sdu, oid) {
-//   try {
-//     report("service about to stop */*", oid,  "others");
-//     report("sending danger signal", oid, "others");
-//     spkgUtils.end(sdu, true, ws);
-//   } catch(error) { return errorResponseHelper(error, oid); }
-// }
+function HEXDECIBINRGB(type, sdu, oid, res) {
+  if (
+    sdu.method === "POST" &&
+    sdu.payload !== null &&
+    Array.isArray(sdu.payload)
+  ) {
+    let response = spkgUtils[type](sdu.payload[0]);
+    finish(200, code_0(true, stdsignal, oid, response), res);
+  } else finish(200, code_0(true, stdsignal, oid, methodpost), res);
+}
+
+async function QUICK(oid, sdu, res) {
+  try {
+    if (
+      sdu.method === "POST" &&
+      sdu.payload !== null &&
+      Array.isArray(sdu.payload) &&
+      sdu.payload.length === 2
+    ) {
+      const path = join(sdu.servicePath, sdu.payload[0]);
+      await spkgUtils.quick(path, sdu.payload[1]);
+      finish(200, code_0(true, "ACK", oid, null), res);
+    } else
+      throw {
+        message:
+          "quick expected method post, with payload [string: path, string: template]",
+      };
+  } catch (error) {
+    finish(200, errorResponseHelper(error, oid), res);
+  }
+}
+
+function END(sdu, oid, res) {
+  report("service about to stop */*", oid, "others");
+  report("sending danger signal", oid, "others");
+  spkgUtils.end(sdu, false);
+  finish(200, code_0(true, "ACK", oid, null), res);
+}
 
 /** a function that handles service api requests and responses for service packages
  * @author david, pass-me-dachips
@@ -60,66 +91,121 @@
  * @returns {void}
  */
 export default async function spkg(opera, oid, sdu, res) {
-  //   const { OPERA, PAYLOAD, OID } = request;
-  //   switch (OPERA) {
-  //      case "ADD" : ws.send(ADDSUBDIVMULTIPLYMODPOW("add", PAYLOAD, OID)); break
-  //      case "SUB" : ws.send(ADDSUBDIVMULTIPLYMODPOW("sub", PAYLOAD, OID)); break
-  //      case "DIV" : ws.send(ADDSUBDIVMULTIPLYMODPOW("div", PAYLOAD, OID)); break
-  //      case "MULTIPLY" :
-  //         ws.send(ADDSUBDIVMULTIPLYMODPOW("multiply", PAYLOAD, OID)); break
-  //      case "MOD" : ws.send(ADDSUBDIVMULTIPLYMODPOW("mod", PAYLOAD, OID)); break
-  //      case "POW" : ws.send(ADDSUBDIVMULTIPLYMODPOW("pow", PAYLOAD, OID)); break
-  //      case "UINT" : ws.send(INTEGERSANDBASICBYTES("uint", PAYLOAD, OID)); break
-  //      case "INT" : ws.send(INTEGERSANDBASICBYTES("int", PAYLOAD, OID)); break
-  //      case "BYTES" : ws.send(INTEGERSANDBASICBYTES("bytes", PAYLOAD, OID)); break
-  //      case "FREE" :
-  //        ws.send(JSON.stringify(code_0(true, stdsignal,OID,spkgUtils.free)));
-  //        break
-  //      case "DIRSIZE" :
-  //        ws.send(JSON.stringify(code_0(true, stdsignal,OID,spkgUtils.defDirSize)));
-  //        break
-  //      case "MARSHAL" :
-  //        ws.send(JSON.stringify(code_0(
-  //           true, stdsignal,
-  //           OID,
-  //           await spkgUtils.marshal(PAYLOAD)
-  //        )));
-  //        break
-  //      case "HMR" :
-  //        ws.send(JSON.stringify(code_0(true, stdsignal,OID,spkgUtils.hmr(PAYLOAD))));
-  //        break
-  //      case "OSPATH" :
-  //        ws.send(JSON.stringify(code_0(
-  //           true, stdsignal,
-  //           OID,
-  //           await spkgUtils.osPath(PAYLOAD.os, PAYLOAD.path)
-  //        )));
-  //        break
-  //      case "ASCII" :
-  //        ws.send(JSON.stringify(code_0(true, stdsignal,OID,spkgUtils.ascii)));
-  //        break
-  //      case "ANSI" :
-  //        ws.send(JSON.stringify(code_0(
-  //           true, stdsignal,
-  //           OID,
-  //           spkgUtils.ansiCodesForColouredOutput
-  //        )));
-  //        break
-  //      case "HEX" : ws.send(HEXDECIBINRGB("hex", PAYLOAD, OID)); break
-  //      case "DECI" : ws.send(HEXDECIBINRGB("deci", PAYLOAD, OID)); break
-  //      case "BIN" : ws.send(HEXDECIBINRGB("bin", PAYLOAD, OID)); break
-  //      case "RGB" : ws.send(HEXDECIBINRGB("rgb", PAYLOAD, OID)); break
-  //      case "QUICK" : ws.send(await QUICK(PAYLOAD, OID, sdu)); break
-  //      case "END" : END(ws, sdu, OID); break
-  //      case "TECHSTACK" :
-  //        ws.send(JSON.stringify(code_0(
-  //           true, stdsignal, OID,
-  //           await spkgUtils.getTechStack(PAYLOAD === "true" ? true : false)
-  //        )));
-  //        break
-  //      case "SERVICEINFO" :
-  //        ws.send(JSON.stringify(code_0( true, stdsignal, OID, sdu )));
-  //        break
-  //      default: ws.send(defopera(OPERA)); //would change.
-  //   };
+  switch (opera.toUpperCase()) {
+    case "ADD":
+      ADDSUBDIVMULTIPLYMODPOW("add", sdu, oid, res);
+      break;
+    case "SUB":
+      ADDSUBDIVMULTIPLYMODPOW("sub", sdu, oid, res);
+      break;
+    case "DIV":
+      ADDSUBDIVMULTIPLYMODPOW("div", sdu, oid, res);
+      break;
+    case "MULTIPLY":
+      ADDSUBDIVMULTIPLYMODPOW("multiply", sdu, oid, res);
+      break;
+    case "MOD":
+      ADDSUBDIVMULTIPLYMODPOW("mod", sdu, oid, res);
+      break;
+    case "POW":
+      ADDSUBDIVMULTIPLYMODPOW("pow", sdu, oid, res);
+      break;
+    case "UINT":
+      INTEGERSANDBASICBYTES("uint", sdu, oid, res);
+      break;
+    case "INT":
+      INTEGERSANDBASICBYTES("int", sdu, oid, res);
+      break;
+    case "BYTES":
+      INTEGERSANDBASICBYTES("bytes", sdu, oid, res);
+      break;
+    case "FREE":
+      finish(200, code_0(true, stdsignal, oid, spkgUtils.free), res);
+      break;
+    case "DIRSIZE":
+      finish(200, code_0(true, stdsignal, oid, spkgUtils.defDirSize), res);
+      break;
+    case "MARSHAL":
+      MARSHAL(sdu, oid, res);
+      break;
+    case "HMR":
+      finish(
+        200,
+        code_0(
+          true,
+          stdsignal,
+          oid,
+          spkgUtils.hmr(Array.isArray(sdu.payload) ? sdu.payload[0] : 0)
+        ),
+        res
+      );
+      break;
+    case "OSPATH":
+      Array.isArray(sdu.payload) &&
+      sdu.payload.length === 2 &&
+      sdu.method === "POST" &&
+      sdu.payload[0] !== null &&
+      sdu.payload[1] !== null
+        ? finish(
+            200,
+            code_0(
+              true,
+              stdsignal,
+              oid,
+              await spkgUtils.osPath(sdu.payload[0], sdu.payload[1])
+            ),
+            res
+          )
+        : finish(
+            200,
+            code_0(
+              true,
+              stdsignal,
+              oid,
+              "expected payload of type array[2] and method post"
+            ),
+            res
+          );
+      break;
+    case "ASCII":
+      finish(200, code_0(true, stdsignal, oid, spkgUtils.ascii), res);
+      break;
+    case "ANSI":
+      finish(
+        200,
+        code_0(true, stdsignal, oid, spkgUtils.ansiCodesForColouredOutput),
+        res
+      );
+      break;
+    case "HEX":
+      HEXDECIBINRGB("hex", sdu, oid, res);
+      break;
+    case "DECI":
+      HEXDECIBINRGB("deci", sdu, oid, res);
+      break;
+    case "BIN":
+      HEXDECIBINRGB("bin", sdu, oid, res);
+      break;
+    case "RGB":
+      HEXDECIBINRGB("rgb", sdu, oid, res);
+      break;
+    case "QUICK":
+      await QUICK(oid, sdu, res);
+      break;
+    case "END":
+      END(sdu, oid, res);
+      break;
+    case "TECHSTACK":
+      finish(
+        200,
+        code_0(true, stdsignal, oid, await spkgUtils.getTechStack(true)),
+        res
+      );
+      break;
+    case "SERVICEINFO":
+      finish(200, code_0(true, stdsignal, oid, sdu), res);
+      break;
+    default:
+      finish(400, code_1(`unknown operation: ${opera}`), res);
+  }
 }
