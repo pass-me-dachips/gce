2d79acba548b883ffccb45bbdbfa9f53ca419d48
pass-me-dachips <da40au@gmail.com>
2024-05-31 13:30:47 +0100

    updated installer

--- run/install.mjs
+++ run/install.mjs
@@ -1,65 +1,71 @@
 "use strict";
 
-/** the program that install :
- *     moves the gce source code and binary to the appropriate location.
+/** the program that installs :
+ *  moves the gce source code and binary to the appropriate location, and install
+ *  the default gcce if supported.
  * @author david, pass-me-dachips
  */
 
 import { copyFileSync, cpSync } from "node:fs";
-import { join } from "node:path";
-import { homedir, platform } from "node:os";
 import { execSync } from "node:child_process";
+import { homedir, platform } from "node:os";
+import { join } from "node:path";
+
+const supportedPlatforms = ["android", "linux", "win32", "darwin"];
 
 const osplatform = platform();
 const oshomedir = homedir();
+const cwd = process.cwd();
 
-const shared_helper = join(oshomedir, "gce", "shared");
-const _shared_path = {
-  android: "/data/data/com.termux/files/home/gce/shared",
-  darwin: shared_helper,
-  linux: shared_helper,
-  win32: shared_helper,
-};
+if (supportedPlatforms.includes) {
+  const shared = join(oshomedir, "gce", "shared");
+  const sharedPath = {
+    android: "/data/data/com.termux/files/home/gce/shared",
+    darwin: shared,
+    linux: shared,
+    win32: shared,
+  };
 
-const local_shared_path = join(process.cwd(), "shared");
-cpSync(local_shared_path, _shared_path[osplatform], { recursive: true });
-// copies the source code to the appropriate destination: home/gce/shared.
+  const localSharedPath = join(cwd, "shared");
+  cpSync(localSharedPath, sharedPath[osplatform], { recursive: true });
+  // copies the source code to the appropriate destination: [home]/gce/shared.
 
-const local_exec_path = join(process.cwd(), "gce");
-if (osplatform === "win32") local_exec_path = local_exec_path + ".exe";
+  let localBin = join(cwd, "gce");
+  if (osplatform === "win32") localBin = localBin + ".exe";
 
-const _os_exec_path = {
-  android: "/data/data/com.termux/files/usr/bin/gce",
-  darwin: "/usr/local/bin/gce",
-  linux: "/usr/local/bin/gce",
-  win32: "C:\\System32\\gce",
-};
-if (osplatform === "linux" || osplatform === "darwin") {
-  const command = `sudo cp ${local_exec_path} ${_os_exec_path[osplatform]}`;
-  execSync(command);
-  // on posix, due to permission resitrictions, instead of copying the file
-  // using node, the installer runs the command that allows the `superuser` to copy
-  // the file instead: you might be promted to input your password
-} else {
-  copyFileSync(local_exec_path, _os_exec_path[osplatform]);
-}
-// copies the gce executable to the appropriate destination.
+  const osUsrBinPath = {
+    android: "/data/data/com.termux/files/usr/bin/gce",
+    darwin: "/usr/local/bin/gce",
+    linux: "/usr/local/bin/gce",
+    win32: "C:\\System32\\gce",
+  };
+  // if (osplatform === "linux" || osplatform === "darwin") {
+  //   const command = `sudo cp ${local_exec_path} ${_os_exec_path[osplatform]}`;
+  //   execSync(command);
+  //   // on posix, due to permission resitrictions, instead of copying the file
+  //   // using node, the installer runs the command that allows the `superuser` to copy
+  //   // the file instead: you might be promted to input your password
+  // } else {
+  //   copyFileSync(local_exec_path, _os_exec_path[osplatform]);
+  // }
+  // // copies the gce executable to the appropriate destination.
 
-console.log(`
-  ██████╗  ██████╗███████╗
- ██╔════╝ ██╔════╝██╔════╝
- ██║  ███╗██║     █████╗  
- ██║   ██║██║     ██╔══╝  
- ╚██████╔╝╚██████╗███████╗
-  ╚═════╝  ╚═════╝╚══════╝
-`);
-console.log("grand code environment v1.0.0");
-console.log(
-  "\x1b[92mcompleted installation! run `gce` for confirmation.\x1b[0m"
-);
-console.log(
-  "need quick help? run `\x1b[92mgce --help\x1b[0m` for more information."
-);
-console.log(
-  "want to start a service? run `\x1b[92mgce <relative/path/to/file/or/dir>\x1b[0m`"
-);
+  // console.log(`
+  //   ██████╗  ██████╗███████╗
+  //  ██╔════╝ ██╔════╝██╔════╝
+  //  ██║  ███╗██║     █████╗
+  //  ██║   ██║██║     ██╔══╝
+  //  ╚██████╔╝╚██████╗███████╗
+  //   ╚═════╝  ╚═════╝╚══════╝
+  // `);
+  // console.log("grand code environment v1.0.0");
+  // console.log(
+  //   "\x1b[92mcompleted installation! run `gce` for confirmation.\x1b[0m"
+  // );
+  // console.log(
+  //   "need quick help? run `\x1b[92mgce --help\x1b[0m` for more information."
+  // );
+  // console.log(
+  //   "want to start a service? run `\x1b[92mgce <relative/path/to/file/or/dir>\x1b[0m`"
+  // );
+} else console.log("unsupported platform %s", osplatform);
