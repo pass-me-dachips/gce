b6d631e1ee981c1e531ec43af7889771556799a6
pass-me-dachips <da40au@gmail.com>
2024-06-01 09:24:10 +0100

    updated variables

--- src/var/osPaths.js
+++ src/var/osPaths.js
@@ -1,45 +1,46 @@
-
 "use strict";
 
-import { homedir, platform as osplatform  } from "node:os";
+import { homedir, platform as osplatform } from "node:os";
 
 export const supportedPlatforms = [
-   "android",
-   "darwin", //mac
-   "linux",
-   "win32" //windows
+  "android",
+  "darwin", //mac
+  "linux",
+  "win32", //windows
 ];
 
 export const osHomeDir = () => {
-   if (supportedPlatforms.includes(osplatform())) {
-     return osplatform() === "android" ? "~/" : homedir();
-   } else throw { color_code: 91, message: "PLATFORM UNSUPPORTED" }
-}
+  if (supportedPlatforms.includes(osplatform())) {
+    return osplatform() === "android"
+      ? "/data/data/com.termux/files/home/"
+      : homedir();
+  } else throw { color_code: 91, message: "PLATFORM UNSUPPORTED" };
+};
 
 export const osConfigDir = {
-   android: [".config"],
-   darwin: ["Library", "Preferences"],
-   linux: [".config"],
-   win32: ["AppData", "Roaming"],
-}
+  android: [".config"],
+  darwin: ["Library", "Preferences"],
+  linux: [".config"],
+  win32: ["AppData", "Roaming"],
+};
 
 export const osDataDir = {
-   android: [".local"],
-   darwin: ["Library", "Application Support"],
-   linux: [".local", "share"],
-   win32: ["AppData", "Local"],
-}
+  android: [".local"],
+  darwin: ["Library", "Application Support"],
+  linux: [".local", "share"],
+  win32: ["AppData", "Local"],
+};
 
 export const osTempDir = {
-   android: [".cache"],
-   darwin: ["Library", "Caches"],
-   linux: [".cache"],
-   win32: ["AppData", "Local", "Temp"],
-}
+  android: [".cache"],
+  darwin: ["Library", "Caches"],
+  linux: [".cache"],
+  win32: ["AppData", "Local", "Temp"],
+};
 
 export const osSeperator = {
-   android: "/",
-   darwin: "/",
-   linux: "/",
-   win32: "\\",
-}
+  android: "/",
+  darwin: "/",
+  linux: "/",
+  win32: "\\",
+};
