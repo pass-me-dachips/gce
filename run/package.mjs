a62e5bbd1548b00e79f13a715c5f24aea2eab2ac
pass-me-dachips <da40au@gmail.com>
2024-06-01 10:16:50 +0100

    updated logs

--- run/package.mjs
+++ run/package.mjs
@@ -8,25 +8,24 @@ import { copyFileSync, cpSync } from "node:fs";
 import { execSync } from "node:child_process";
 import { join } from "node:path";
 
-const shared_path = join("distrib", "shared");
+const sharedPath = join("distrib", "shared");
 
-console.log("copying shared code: ");
-cpSync("src", join(shared_path, "src"), { recursive: true });
-cpSync("node_modules", join(shared_path, "node_modules"), { recursive: true });
-cpSync("man", join(shared_path, "man"), { recursive: true });
-copyFileSync("package.json", join(shared_path, "package.json"));
-console.log("\x1b[92mcopied shared code successfully!\n\x1b[0m");
-console.log("copying helpers: ");
+console.log("[-] Copying the source code");
+cpSync("src", join(sharedPath, "src"), { recursive: true });
+cpSync("node_modules", join(sharedPath, "node_modules"), { recursive: true });
+cpSync("man", join(sharedPath, "man"), { recursive: true });
+copyFileSync("package.json", join(sharedPath, "package.json"));
+console.log("\x1b[92m[-] Copied the source code successfully!\n\x1b[0m");
 // copies the source code to distrib/shared
 
-const gce_path = join("run", "gce.nim");
-const compile_command = `nim c -d:release --app:console --outDir:distrib --opt:speed ${gce_path}`;
-execSync(compile_command);
-const install_path = join("run", "install.mjs");
-copyFileSync(install_path, join("distrib", "install.mjs"));
-console.log("\x1b[92mcopied helpers successfully!\n\x1b[0m");
-// copies the helpers: gce, install.mjs to distrib
+console.log("[-] Copying Helper Files ");
+const gcePath = join("run", "gce.nim");
+const compileCommand = `nim c -d:release --app:console --outDir:distrib --opt:speed ${gcePath}`;
+execSync(compileCommand);
 
-console.log(
-  `\x1b[95mcompleted packaging!. go to /distrib to see results.\x1b[0m`
-);
+const installPath = join("run", "install.mjs");
+copyFileSync(installPath, join("distrib", "install.mjs"));
+console.log("\x1b[92m[-] Copied Helper Files successfully!\n\x1b[0m");
+// copies the helper files: gce, install.mjs to distrib
+
+console.log(`\x1b[95m~! Go to /distrib to see results.\x1b[0m`);
