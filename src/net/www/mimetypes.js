b8b23e9e919855bbd3c459baef6d29209226df6e
pass-me-dachips <da40au@gmail.com>
2024-06-05 12:09:08 +0100

    updated the mime types/subtypes

--- src/net/www/mimetypes.js
+++ src/net/www/mimetypes.js
@@ -1,4 +1,4 @@
-/*
+/**
  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    This file determines the mime type for HTTP responses based on the 
    requested file extension sent from the client.
@@ -16,7 +16,8 @@ export default function mime(trail) {
   let type_subtype;
   const extention = (n) => trail.endsWith(n);
   if (extention(".7z")) type_subtype = "application/x-7z-compressed";
-  else if (extention(".js")) type_subtype = "application/javascript";
+  else if (extention(".js") || extention(".mjs") || extention(".cjs"))
+    type_subtype = "application/javascript";
   else if (extention(".jpg") || extention(".jpeg")) type_subtype = "image/jpeg";
   else if (extention(".ico")) type_subtype = "image/x-icon";
   else if (extention(".html") || extention(".htm")) type_subtype = "text/html";
@@ -26,23 +27,19 @@ export default function mime(trail) {
   else if (extention(".ttf")) type_subtype = "font/ttf";
   else if (extention(".csv")) type_subtype = "text/csv";
   else if (extention(".svg")) type_subtype = "image/svg+xml";
-  else if (
-    extention(".mpeg") ||
-    extention(".mpg") ||
-    extention(".mp2") ||
-    extention(".mp3") ||
-    extention(".mpga")
-  )
-    type_subtype = "video/mpeg";
+  else if (extention(".mpeg") || extention(".mpg")) type_subtype = "video/mpeg";
+  else if (extention(".mp2") || extention(".mp3") || extention(".mpga"))
+    type_subtype = "audio/mpeg";
   else if (extention(".mp4")) type_subtype = "video/mp4";
   else if (extention(".zip")) type_subtype = "application/zip";
   else if (extention(".yml") || extention(".yaml"))
     type_subtype = "application/x-yaml";
-  else if (extention(".xml")) type_subtype = "text/xml";
+  else if (extention(".xml")) type_subtype = "application/xml";
   else if (extention(".webp")) type_subtype = "image/webp";
   else if (extention(".webm")) type_subtype = "video/webm";
   else if (extention(".wav")) type_subtype = "audio/wav";
-  else if (extention(".viv") || extention(".vivo")) type_subtype = "video/vivo";
+  else if (extention(".viv") || extention(".vivo"))
+    type_subtype = "video/vnd.vivo";
   else if (extention(".tar")) type_subtype = "application/x-tar";
   else if (extention(".svr")) type_subtype = "application/x-world";
   else if (extention(".sprite")) type_subtype = "application/x-sprite";
@@ -52,9 +49,29 @@ export default function mime(trail) {
   else if (extention(".png")) type_subtype = "image/png";
   else if (extention(".pic") || extention(".pict")) type_subtype = "image/pict";
   else if (extention(".gz") || extention(".gzip"))
-    type_subtype = "application/x-gzip";
+    type_subtype = "application/gzip";
   else if (extention(".gif")) type_subtype = "image/gif";
   else if (extention(".env")) type_subtype = "application/envoy";
+  else if (extention(".pdf")) type_subtype = "application/pdf";
+  else if (extention(".doc")) type_subtype = "application/msword";
+  else if (extention(".docx"))
+    type_subtype =
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+  else if (extention(".xls")) type_subtype = "application/vnd.ms-excel";
+  else if (extention(".xlsx"))
+    type_subtype =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+  else if (extention(".ppt")) type_subtype = "application/vnd.ms-powerpoint";
+  else if (extention(".pptx"))
+    type_subtype =
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+  else if (extention(".odt"))
+    type_subtype = "application/vnd.oasis.opendocument.text";
+  else if (extention(".ods"))
+    type_subtype = "application/vnd.oasis.opendocument.spreadsheet";
+  else if (extention(".epub")) type_subtype = "application/epub+zip";
+  else if (extention(".midi") || extention(".mid")) type_subtype = "audio/midi";
+  else if (extention(".bmp")) type_subtype = "image/bmp";
   else type_subtype = "text/plain";
   return type_subtype;
 }
