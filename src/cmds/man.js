e60e940e2c618c3e67cf795b4dc43c665b6507ed
pass-me-dachips <da40au@gmail.com>
2024-06-07 16:58:16 +0100

    removed about and contrib local manuals

--- src/cmds/man.js
+++ src/cmds/man.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 import man from "../etc/man.js";
@@ -8,29 +7,50 @@ const manualPath = (basename) => `./man/${basename}`;
 /**
  * handler for the man command.
  * @author david, pass-me-dachips
- * @param {Array} args 
+ * @param {Array} args
  * @returns {void}
  */
 export default function Man(args) {
- args = args.slice(1);
- if (args.length === 1) {
-   switch(args[0]) {
-      case "about":  ""; break;
-      case "contrib":  ""; break;
-      case "clean":  man(manualPath("clean"), 30); break;
-      case "gcce":  man(manualPath("gcce"), 12); break;
-      case "globalConfig":  man(manualPath("globalConfig"), 30); break;
-      case "install":  man(manualPath("install"), 30); break;
-      case "kill":  man(manualPath("kill"), 25); break;
-      case "pkg":  man(manualPath("pkg"), 30); break;
-      case "remove":  man(manualPath("remove"), 30); break;
-      case "resetGlobalConfig":  man(manualPath("resetGlobalConfig"), 20); break;
-      case "rkill":  man(manualPath("rkill"), 15); break;
-      case "services":  man(manualPath("services"), 30); break;
-      case "start":  man(manualPath("start"), 30); break;
-      default: throw { 
-         message: `gce does not have a manual for command ${args[0]}` 
-      }
-   }
- } else throw { message: "man expects only 1 argument" }
+  args = args.slice(1);
+  if (args.length === 1) {
+    switch (args[0]) {
+      case "clean":
+        man(manualPath("clean"), 30);
+        break;
+      case "gcce":
+        man(manualPath("gcce"), 12);
+        break;
+      case "globalConfig":
+        man(manualPath("globalConfig"), 30);
+        break;
+      case "install":
+        man(manualPath("install"), 30);
+        break;
+      case "kill":
+        man(manualPath("kill"), 25);
+        break;
+      case "pkg":
+        man(manualPath("pkg"), 30);
+        break;
+      case "remove":
+        man(manualPath("remove"), 30);
+        break;
+      case "resetGlobalConfig":
+        man(manualPath("resetGlobalConfig"), 20);
+        break;
+      case "rkill":
+        man(manualPath("rkill"), 15);
+        break;
+      case "services":
+        man(manualPath("services"), 30);
+        break;
+      case "start":
+        man(manualPath("start"), 30);
+        break;
+      default:
+        throw {
+          message: `gce does not have a manual for command ${args[0]}`,
+        };
+    }
+  } else throw { message: "man expects only 1 argument" };
 }
