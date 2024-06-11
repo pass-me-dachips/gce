8c0898bd720c3c16f0fdcc454ce9ef71d0281d71
pass-me-dachips <da40au@gmail.com>
2024-06-11 22:53:30 +0100

    updated packager: added a dedictated folder per platform distrib

--- run/package.mjs
+++ run/package.mjs
@@ -6,9 +6,12 @@
 
 import { copyFileSync, cpSync } from "node:fs";
 import { execSync } from "node:child_process";
+import { platform as osPlatform } from "node:os";
 import { join } from "node:path";
 
-const sharedPath = join("distrib", "shared");
+
+const platform = osPlatform();
+const sharedPath = join("distrib", platform , "shared");
 
 console.log("[-] Copying the source code");
 cpSync("src", join(sharedPath, "src"), { recursive: true });
@@ -20,12 +23,13 @@ console.log("\x1b[92m[-] Copied the source code successfully!\n\x1b[0m");
 
 console.log("[-] Copying Helper Files ");
 const gcePath = join("run", "gce.nim");
-const compileCommand = `nim c -d:release --app:console --outDir:distrib --opt:speed ${gcePath}`;
+const compileCommand = 
+  `nim c -d:release --app:console --outDir:${join("distrib", platform)} --opt:speed ${gcePath}`;
 execSync(compileCommand);
 
 const installPath = join("run", "install.mjs");
-copyFileSync(installPath, join("distrib", "install.mjs"));
+copyFileSync(installPath, join("distrib", platform, "install.mjs"));
 console.log("\x1b[92m[-] Copied Helper Files successfully!\n\x1b[0m");
 // copies the helper files: gce, install.mjs to distrib
 
-console.log(`\x1b[95m~! Go to /distrib to see results.\x1b[0m`);
+console.log(`\x1b[95m~! Go to /distrib/${platform} to see results.\x1b[0m`);
